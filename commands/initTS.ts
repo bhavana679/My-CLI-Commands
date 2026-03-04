20380a998c957491179ae2ef70725cc73e2914ca
bhavana679 <bhavanatalkute@gmail.com>
2026-03-04 22:21:49 +0530

    Add utility commands (weather, country, greet, init_ts)

---
+++ commands/initTS.ts
@@ -0,0 +1,31 @@
+const { execSync } = require("child_process")
+const chalk = require("chalk")
+class Init_TS {
+    program;
+    constructor(program) {
+        this.program = program
+    }
+    register() {
+        this.program
+            .command("init_ts <folderName>")
+            .action((folderName) => this.initializeTS(folderName))
+    }
+    initializeTS(folderName) {
+        console.log(chalk.green("initialize typescript project"))
+        this.run(`mkdir ${folderName}`)
+        this.run(`cd ${folderName} && npm init -y`)
+        this.run(`cd ${folderName} && npm install -D typescript ts-node nodemon @types/node`)
+        this.run(`cd ${folderName} && tsc --init`)
+        console.log(chalk.blue("Typescript done"))
+
+
+    }
+    run(command) {
+        execSync(command, { stdio: "inherit" })
+    }
+}
+
+
+
+module.exports = Init_TS;
+export {};
